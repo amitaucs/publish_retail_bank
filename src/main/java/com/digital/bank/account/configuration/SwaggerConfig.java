cfb7f42f19864efb00feca7d896027f9dd714c1f
amitaucs <the.amisoft@gmail.com>
2020-05-05 18:57:43 +0200

    saving-account

--- src/main/java/com/digital/bank/account/configuration/SwaggerConfig.java
+++ src/main/java/com/digital/bank/account/configuration/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("com.digital.bank.account.controller"))
+                .select()                 .apis(RequestHandlerSelectors.basePackage("com.digital.bank.account.inboundadapter"))
                 .paths(regex("/retailbanking.*"))
                 .build();
     }
