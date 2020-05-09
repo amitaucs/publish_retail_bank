86876f39f474688a063d6baf72520c8c02807bf2
amitaucs <the.amisoft@gmail.com>
2020-05-09 17:24:46 +0200

    Unused dependencies removed also updated

--- src/main/java/com/digital/bank/account/inboundadapter/UserAccountController.java
+++ src/main/java/com/digital/bank/account/inboundadapter/UserAccountController.java
@@ -5,6 +5,7 @@ import com.digital.bank.account.domain.CreateAccountResponse;
 import com.digital.bank.account.domain.UserAccountVO;
 import com.digital.bank.account.domain.port.UserAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@EnableDiscoveryClient
 @CrossOrigin(value = "*")
 @RequestMapping("/account")
 public class UserAccountController {
